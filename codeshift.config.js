56b728071f72f2dcf0c8a1ce81ca0ea62bee9f91
Sam Rose <11774595+samrose3@users.noreply.github.com>
2023-03-09 11:30:59 -0500

    Remove extra metadata from codeshift config

--- codeshift.config.js
+++ codeshift.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  maintainers: [],
-  targets: [],
-  description: 'Codemods for Polaris',
-  transforms: {},
   presets: {
     'react-rename-component-prop': require.resolve('./codemods/react-rename-component-prop/transform'),
   },
