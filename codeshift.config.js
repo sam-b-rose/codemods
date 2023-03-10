689c43b64c20dff783379e218783061e64536163
Sam Rose <11774595+samrose3@users.noreply.github.com>
2023-03-10 16:12:27 -0500

    Add extension to require to support module import

--- codeshift.config.js
+++ codeshift.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   transforms: {
-    'v9-scss-replace-border-radius': require.resolve('./codemods/v9-scss-replace-border-radius/transform')
+    'v9-scss-replace-border-radius': require.resolve('./codemods/v9-scss-replace-border-radius/transform.ts')
   },
   presets: {
-    'react-rename-component-prop': require.resolve('./codemods/react-rename-component-prop/transform'),
+    'react-rename-component-prop': require.resolve('./codemods/react-rename-component-prop/transform.ts'),
   },
 };
