a58029a63c5ec4697368217171b8afdcbbe7fd2b
Sam Rose <11774595+samrose3@users.noreply.github.com>
2023-03-16 15:19:50 -0400

    Revert "Use module type"
    This reverts commit fbcdf0610b6e4520e2d989509756085b937a164e.

--- codeshift.config.js
+++ codeshift.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   transforms: {
-    '9.0.0': require.resolve('./src/codemods/9.0.0/transform.ts')
+    '9.0.0': require.resolve('./src/codemods/9.0.0/transform')
   },
   presets: {
     'v9-scss-replace-border-radius': require.resolve(
-      './src/codemods/9.0.0/v9-scss-replace-border-radius/transform.ts',
+      './src/codemods/9.0.0/v9-scss-replace-border-radius/transform',
     ),
     'react-rename-component-prop': require.resolve(
-      './src/codemods/presets/react-rename-component-prop/transform.ts',
+      './src/codemods/presets/react-rename-component-prop/transform',
     ),
   },
 }
