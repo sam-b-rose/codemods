fbcdf0610b6e4520e2d989509756085b937a164e
Sam Rose <11774595+samrose3@users.noreply.github.com>
2023-03-16 15:15:09 -0400

    Use module type

--- codeshift.config.js
+++ codeshift.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   transforms: {
-    '9.0.0': require.resolve('./src/codemods/9.0.0/transform')
+    '9.0.0': require.resolve('./src/codemods/9.0.0/transform.ts')
   },
   presets: {
     'v9-scss-replace-border-radius': require.resolve(
-      './src/codemods/9.0.0/v9-scss-replace-border-radius/transform',
+      './src/codemods/9.0.0/v9-scss-replace-border-radius/transform.ts',
     ),
     'react-rename-component-prop': require.resolve(
-      './src/codemods/presets/react-rename-component-prop/transform',
+      './src/codemods/presets/react-rename-component-prop/transform.ts',
     ),
   },
 }
